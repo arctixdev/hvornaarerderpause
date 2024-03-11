c642ade971aab5afbae851395df320b5e34328c3
Jonathan Bangert <jonathan@bangert.dk>
2024-03-11 17:18:46 +0100

    Idk aron stuff

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ function getTime() {
     let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
-
+    
     hours = hours - days * 24;
     minutes = minutes - days * 24 * 60 - hours * 60;
     seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
-
+    
+    if(seconds < 10) { seconds = "0" + seconds;}
+    
     console.log(seconds)
 
     return {
